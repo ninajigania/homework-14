348e23b4d508d749321249b8f17649b9f33c6c5e
ninajigania <ninajigania@gmail.com>
2023-01-29 21:44:50 +0400

    update

---
+++ script.js
@@ -0,0 +1,25 @@
+person =  {
+    name : "Nina",
+    age : 18 , 
+    address: "kostava",
+};
+
+console.log(`My name is ${person.name}`);
+console.log(`My age is ${person.age}`);
+console.log(`My address is ${person.address}`)
+
+if (person.age < 19) {
+    console.log("I am a teeneger");
+} else {
+    console.log("I am an adult");
+
+}
+
+let numbers = [11, 22, 33, 44, 55];
+
+console.log(numbers[0]);
+console.log(numbers[1]);
+console.log(numbers[2]);
+console.log(numbers[3]);
+console.log(numbers[4]);
+console.log(numbers);
